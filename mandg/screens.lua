c0f0c22bc0a2a8ec2fe75223b5d24a89e0c19085
balb <1763655+balb@users.noreply.github.com>
2023-12-23 19:26:16 +0000

    shrink

--- mandg/screens.lua
+++ mandg/screens.lua
@@ -1,36 +1,39 @@
 function build_screens()
   local tab = {}
-  tab["00"] = build_screen({ build_old_woman() })
+  tab["00"] = build_screen { build_old_woman() }
   tab["10"] = build_screen_desert_top_firestones()
   tab["20"] = build_screen_desert_fuzzies()
-  tab["30"] = build_screen({
+  tab["30"] =
     -- enter the north dungeon
-    build_firestone(24, 88, 3, 8, 32),
-    build_jazzer(72, 64, 48, 81),
-    build_jazzer(96, 56, 48, 81)
-  })
+    build_screen {
+      build_firestone(24, 88, 3, 8, 32),
+      build_jazzer(72, 64, 48, 81),
+      build_jazzer(96, 56, 48, 81)
+    }
   tab["40"] = build_screen_north_dungeon_fuzzies()
-  tab["50"] = build_screen({
+  tab["50"] =
     -- north dungeon locked door
-    build_door(113, 56, "simple_key"),
-    build_firestone(72, 40, 3, 8, 24),
-    build_firestone(64, 80, 3, 16, 32),
-    build_idiot(24, 96, 8, 88)
-  })
+    build_screen {
+      build_door(113, 56, "simple_key"),
+      build_firestone(72, 40, 3, 8, 24),
+      build_firestone(64, 80, 3, 16, 32),
+      build_idiot(24, 96, 8, 88)
+    }
   -- final screen 60
-  tab["60"] = build_screen({})
+  tab["60"] = build_screen {}
   tab["11"] = build_screen_desert_5_idiots()
-  tab["21"] = build_screen({
+  tab["21"] = build_screen {
     build_idiot(88, 88, 80, 112),
     build_idiot(56, 56, 40, 64)
-  })
+  }
   tab["31"] = build_screen_town_square()
-  tab["41"] = build_screen({
+  tab["41"] =
     -- jungle 1
-    build_jazzer(32, 88, 72, 112),
-    build_idiot(96, 64, 64, 112),
-    build_idiot(96, 80, 72, 112)
-  })
+    build_screen {
+      build_jazzer(32, 88, 72, 112),
+      build_idiot(96, 64, 64, 112),
+      build_idiot(96, 80, 72, 112)
+    }
   tab["51"] = build_screen_north_dungeon_skellington()
   tab["12"] = build_screen_desert_dead_end()
   tab["42"] = build_screen_jungle_2()
@@ -39,14 +42,15 @@ function build_screens()
   tab["03"] = build_screen_desert_fli_boss()
   tab["13"] = build_screen_desert_sand_wall()
   tab["23"] = build_screen_desert_cactus_and_spade()
-  tab["63"] = build_screen({
+  tab["63"] =
     -- jungle firestone walk
-    build_firestone(102, 32, 2, 0, 88),
-    build_firestone(96, 48, 2, 8, 80),
-    build_firestone(64, 64, 2, 3, 48),
-    build_firestone(96, 80, 2, 8, 80),
-    build_firestone(102, 96, 2, 0, 88)
-  })
+    build_screen {
+      build_firestone(102, 32, 2, 0, 88),
+      build_firestone(96, 48, 2, 8, 80),
+      build_firestone(64, 64, 2, 3, 48),
+      build_firestone(96, 80, 2, 8, 80),
+      build_firestone(102, 96, 2, 0, 88)
+    }
   tab["73"] = build_screen_jungle_monkey()
   return tab
 end
@@ -107,7 +111,7 @@ function build_screen_desert_top_firestones()
 end
 
 function build_screen_desert_fuzzies()
-  local path1, path2 = split_path("24,24 88,24 88,56 24,56"), split_path("80,72 24,72 24,96 80,96")
+  local path1, path2 = split_path "24,24 88,24 88,56 24,56", split_path "80,72 24,72 24,96 80,96"
   return build_screen {
     build_fuzzy(24, 24, path1, 2),
     build_fuzzy(56, 24, path1, 2),
@@ -134,7 +138,7 @@ function build_screen_desert_5_idiots()
 end
 
 function build_screen_desert_dead_end()
-  local path = split_path("80,40 104,40 104,64 80,64")
+  local path = split_path "80,40 104,40 104,64 80,64"
   return build_screen {
     build_fuzzy(80, 40, path, 1),
     build_fuzzy(104, 64, path, 3),
@@ -147,22 +151,22 @@ function build_screen_desert_sand_wall()
   return build_screen {
     build_idiot(112, 48, 72, 112),
     build_idiot(72, 80, 72, 112),
-    build_fuzzy(24, 32, split_path("24,32, 40,32 40,56 24,56"), 1),
-    build_fuzzy(40, 104, split_path("24,80, 40,80 40,104 24,104"), 3),
+    build_fuzzy(24, 32, split_path "24,32, 40,32 40,56 24,56", 1),
+    build_fuzzy(40, 104, split_path "24,80, 40,80 40,104 24,104", 3),
     build_sandwall()
   }
 end
 
 function build_screen_desert_cactus_and_spade()
   return build_screen {
-    build_thrower("cactus", 88, 24, split_path("24,24 88,24 88,96 24,96"), 2),
+    build_thrower("cactus", 88, 24, split_path "24,24 88,24 88,96 24,96", 2),
     build_spade(96, 24)
   }
 end
 
 function build_screen_desert_fli_boss()
   local fli = build_fli()
-  local screen = build_screen({ fli })
+  local screen = build_screen { fli }
   screen.boss_fli = fli
   screen:add_ent(build_textbox2(
     {
@@ -183,14 +187,14 @@ function build_screen_jungle_2()
     build_firestone(56, 40, 2, 14, 40),
     build_firestone(64, 80, 1, 2, 24),
     build_idiot(96, 40, 72, 104),
-    build_fuzzy(72, 48, split_path("72,48 96,48 96,72 72,72"), 1),
+    build_fuzzy(72, 48, split_path "72,48 96,48 96,72 72,72", 1),
     build_idiot(32, 48, 8, 48),
     build_jazzer(56, 88, 88, 112)
   }
 end
 
 function build_screen_jungle_lake_fuzzies()
-  local path1, screen = split_path("48,64 72,64 72,80 80,80 80,104 40,104 40,96 32,96 32,80 40,80 40,72 48,72"), build_screen {}
+  local path1, screen = split_path "48,64 72,64 72,80 80,80 80,104 40,104 40,96 32,96 32,80 40,80 40,72 48,72", build_screen {}
   foreach(
     { 3, 5, 7, 9, 11 },
     function(i)
@@ -221,13 +225,13 @@ end
 
 function build_screen_jungle_monkey()
   return build_screen {
-    build_thrower("monkey", 88, 24, split_path("24,32 88,32 88,96 24,96"), 2),
+    build_thrower("monkey", 88, 24, split_path "24,32 88,32 88,96 24,96", 2),
     build_bra(96, 24)
   }
 end
 
 function build_screen_north_dungeon_fuzzies()
-  local path1, path2 = split_path("88,72 32,72 32,104 88,104"), split_path("88,24 32,24 32,56 88,56")
+  local path1, path2 = split_path "88,72 32,72 32,104 88,104", split_path "88,24 32,24 32,56 88,56"
   return build_screen {
     build_fuzzy(32, 72, path1, 3),
     build_fuzzy(88, 104, path1, 1),
@@ -238,7 +242,7 @@ end
 
 function build_screen_north_dungeon_skellington()
   return build_screen {
-    build_thrower("skellington", 88, 32, split_path("24,32 88,32 88,100 24,100"), 2),
+    build_thrower("skellington", 88, 32, split_path "24,32 88,32 88,100 24,100", 2),
     build_simple_key()
   }
 end

999a6634f018e6ca67aafa6ef1fcc227b61017f1
balb <1763655+balb@users.noreply.github.com>
2023-12-24 16:23:29 +0000

    shrink

--- mandg/map.lua
+++ mandg/map.lua
@@ -1,14 +1,7 @@
--- todo: del_wall func?
-
 function map_remove_desert_top_wall()
-    mset(20, 15, 64)
-    mset(21, 15, 64)
-    mset(22, 15, 64)
-    mset(23, 15, 64)
-    mset(24, 15, 64)
-    mset(25, 15, 64)
-    mset(26, 15, 64)
-    mset(27, 15, 64)
+    for celx = 20, 27 do
+        mset(celx, 15, 64)
+    end
 end
 
 function map_add_desert_town_square_wall()
